bdb3edf125ac854a3c895105c70c03f67e9dde0f
Newton Truong <n3wtn9@gmail.com>
2018-03-13 17:53:26 -0700

    added delete impl to bst

--- src/main/java/datastructures/BinarySearchTree.java
+++ src/main/java/datastructures/BinarySearchTree.java
@@ -3,22 +3,130 @@ package datastructures;
 public class BinarySearchTree {
     Node root;
 
-    public BinarySearchTree(int key) {
-        root = new Node(key);
+    Node search(Node n, int key) {
+        if (n == null) return null;
+
+        if (key < n.key)
+            return search(n.left, key);
+        else if (key > n.key)
+            return search(n.right, key);
+        else
+            return n;
     }
 
-    Node search(int key) {}
-    int minimum() {}
-    int maximum() {}
-    Node predecessor(int key) {}
-    Node successor(int key) {}
-    void insert(int key) {}
-    void delete(int key) {}
-    void trim(int low, int high) {}
+    Node minimum(Node n) {
+        while (n.left != null)
+            n = n.left;
+        return n;
+    }
+
+    Node maximum(Node n) {
+        while(n.right != null)
+            n = n.right;
+        return n;
+    }
+
+    Node successor(Node n) {
+        if (n.right != null)
+            return minimum(n.right);
+
+        Node p = n.parent;
+        while (p != null && p.right == n) {
+            n = p;
+            p = n.parent;
+        }
+        return p;
+    }
+
+    // no parent
+    Node secessor2(Node n) { return n; }
+
+    Node predecessor(Node n) {
+        if (n.left != null)
+            return maximum(n.right);
+
+        Node p = n.parent;
+        while (p != null && p.left == n) {
+            n = p;
+            p = n.parent;
+        }
+        return p;
+    }
+
+    // no parent
+    Node predecessor2(Node n) { return n; }
+
+    void insert(int key) {
+        if (root == null) {
+            root = new Node(key);
+            return;
+        }
+
+        Node next = root;
+        Node parent;
+        do {
+            parent = next;
+            if (key < next.key)
+                next = next.left;
+            else if (key > next.key)
+                next = next.right;
+            else
+                return;
+        }
+        while (next != null);
+
+        if (key < parent.key) {
+            parent.left = new Node(key);
+            parent.left.parent = parent;
+        } else {
+            parent.right = new Node(key);
+            parent.right.parent = parent;
+        }
+    }
+
+    void delete(Node n) {
+        if (n.left == null) {
+            transplant(n, n.right);
+        } else if (n.left != null) {
+            transplant(n, n.left);
+        } else {
+            Node min = minimum(n.right);
+            if (min.parent != n) {
+                transplant(min, min.right);
+                min.right = n.right;
+                min.right.parent = min;
+            }
+            transplant(n, min);
+            min.left = n.left;
+            min.left.parent = min;
+        }
+    }
+
+    void transplant(Node u, Node v) {
+        if (u.parent == null) {
+            root = v;
+            return;
+        }
+
+        if (u == u.parent.right)
+            u.parent.right = v;
+        else
+            u.parent.left = v;
+
+        if (v != null)
+            v.parent = u.parent;
+    }
+
+    Node trim(Node n, int low, int high) {}
+
+    void leftRotate() {}
+
+    void rightRotate() {}
 
     static class Node {
         int key;
-        Node left, right;
+        Node left, right, parent;
+
         public Node(int key) {
             this.key = key;
         }
